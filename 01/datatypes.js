c294f329a6d22f84b1ed4d2e7a583d7901d5c41b
simmarr <128778351+simmarr@users.noreply.github.com>
2024-04-26 13:30:18 +0000

    nothing

---
+++ 01/datatypes.js
@@ -0,0 +1,5 @@
+let name="Simar"
+let age=22
+let male=true
+let occupation;
+console.table([typeof name,typeof age,typeof male,typeof occupation])
---
+++ 01/variables.js
@@ -0,0 +1,6 @@
+let name ="Simar"
+var rollNo =102003072
+const sex="MALE"
+
+console.log(name);
+console.table([name,rollNo,sex]);
